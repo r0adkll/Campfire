a905c1f4e01a669e8ccfef6beecea2be7c194a5d
r0adkll <veedubusc@gmail.com>
2024-12-17 17:28:04 -0500

    Fixing secondary text on playback bar

--- features/sessions/ui/src/commonMain/kotlin/app/campfire/sessions/ui/CollapsedPlaybackBar.kt
+++ features/sessions/ui/src/commonMain/kotlin/app/campfire/sessions/ui/CollapsedPlaybackBar.kt
@@ -240,7 +240,7 @@ private fun CollapsedPlaybackBarContent(
         Text(
           text = subtitle,
           style = MaterialTheme.typography.labelSmall,
-          color = MaterialTheme.typography.labelSmall.color.copy(0.7f),
+          modifier = Modifier.alpha(0.7f)
         )
       }
 
