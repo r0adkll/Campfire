6789c4aaf540e332cd5c658e9b6adcaee1530056
r0adkll <veedubusc@gmail.com>
2025-07-06 21:28:50 -0400

    Fixed widget performance Fixed bug with some chapter <> track association

--- ui/widgets/api/src/commonMain/kotlin/app/campfire/widgets/WidgetUpdater.kt
+++ ui/widgets/api/src/commonMain/kotlin/app/campfire/widgets/WidgetUpdater.kt
@@ -1,9 +1,15 @@
 package app.campfire.widgets
 
+import kotlin.time.Duration
+
 interface WidgetUpdater {
 
   /**
    * Update any homescreen widgets with the lastest information from the app.
    */
-  suspend fun updatePlayerWidget()
+  suspend fun updatePlayerWidget(
+    currentTime: Duration? = null,
+    currentDuration: Duration? = null,
+    playbackSpeed: Float? = null,
+  )
 }
--- ui/widgets/impl/src/androidMain/kotlin/app/campfire/widgets/AndroidWidgetUpdater.kt
+++ ui/widgets/impl/src/androidMain/kotlin/app/campfire/widgets/AndroidWidgetUpdater.kt
@@ -1,29 +1,32 @@
 package app.campfire.widgets
 
 import android.app.Application
-import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.state.updateAppWidgetState
 import app.campfire.core.di.AppScope
-import app.campfire.core.time.FatherTime
+import app.campfire.core.extensions.asSeconds
 import com.r0adkll.kimchi.annotations.ContributesBinding
+import kotlin.time.Duration
 import me.tatarka.inject.annotations.Inject
 
 @ContributesBinding(AppScope::class)
 @Inject
 class AndroidWidgetUpdater(
   private val application: Application,
-  private val fatherTime: FatherTime,
 ) : WidgetUpdater {
 
-  override suspend fun updatePlayerWidget() {
-//    PlayerWidget().updateAll(application)
-
+  override suspend fun updatePlayerWidget(
+    currentTime: Duration?,
+    currentDuration: Duration?,
+    playbackSpeed: Float?,
+  ) {
     // Update the widget with the new data
     val glanceIds = GlanceAppWidgetManager(application).getGlanceIds(PlayerWidget::class.java)
     glanceIds.forEach { id ->
-      updateAppWidgetState(application, id) {
-        it[longPreferencesKey("now")] = fatherTime.nowInEpochMillis()
+      updateAppWidgetState(application, id) { state ->
+        currentTime?.let { state[PlayerWidget.KEY_CURRENT_TIME] = it.asSeconds() }
+        currentDuration?.let { state[PlayerWidget.KEY_CURRENT_DURATION] = it.asSeconds() }
+        playbackSpeed?.let { state[PlayerWidget.KEY_PLAYBACK_SPEED] = it }
       }
       PlayerWidget().update(application, id)
     }
