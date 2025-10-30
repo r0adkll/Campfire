045dc23d64239ae6884410f19543993299aefb82
r0adkll <veedubusc@gmail.com>
2025-10-30 11:59:15 -0400

    Docs

--- app/baselineprofile/src/main/java/app/campfire/baselineprofile/BaselineProfileGenerator.kt
+++ app/baselineprofile/src/main/java/app/campfire/baselineprofile/BaselineProfileGenerator.kt
@@ -13,27 +13,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
- * This test class generates a basic startup baseline profile for the target package.
- *
- * We recommend you start with this but add important user flows to the profile to improve their performance.
- * Refer to the [baseline profile documentation](https://d.android.com/topic/performance/baselineprofiles)
- * for more information.
- *
- * You can run the generator with the "Generate Baseline Profile" run configuration in Android Studio or
- * the equivalent `generateBaselineProfile` gradle task:
- * ```
- * ./gradlew :app.android:generateReleaseBaselineProfile
- * ```
- * The run configuration runs the Gradle task and applies filtering to run only the generators.
- *
- * Check [documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args)
- * for more information about available instrumentation arguments.
- *
- * After you run the generator, you can verify the improvements running the [StartupBenchmarks] benchmark.
- *
- * When using this class to generate a baseline profile, only API 33+ or rooted API 28+ are supported.
- *
- * The minimum required version of androidx.benchmark to generate a baseline profile is 1.2.0.
+ * This test class generates a baseline startup baseline profile for the target package as well
+ * as startup profiles for dex layout optimization.
  **/
 @RunWith(AndroidJUnit4::class)
 @LargeTest
