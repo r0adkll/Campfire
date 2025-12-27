47abd9b24c1baeda17b8c9332d97e814b11d685f
r0adkll <veedubusc@gmail.com>
2025-12-27 14:19:57 -0500

    Fixing desktop/ios bindings for now

--- data/network/oidc/src/jvmMain/kotlin/app/campfire/network/oidc/di/OpenIdAuthComponent.jvm.kt
+++ data/network/oidc/src/jvmMain/kotlin/app/campfire/network/oidc/di/OpenIdAuthComponent.jvm.kt
@@ -1,3 +1,14 @@
 package app.campfire.network.oidc.di
 
-actual interface PlatformOpenIdAuthComponent
+import app.campfire.network.oidc.AuthorizationFlow
+import app.campfire.network.oidc.OpenIdAuthorization
+import me.tatarka.inject.annotations.Provides
+
+actual interface PlatformOpenIdAuthComponent {
+  @Provides
+  fun provideDesktopAuthorizationFlow(): AuthorizationFlow = object : AuthorizationFlow {
+    override suspend fun getAuthorization(serverUrl: String): Result<OpenIdAuthorization> {
+      return Result.failure(IllegalStateException("OAuth not supported on this platform"))
+    }
+  }
+}
