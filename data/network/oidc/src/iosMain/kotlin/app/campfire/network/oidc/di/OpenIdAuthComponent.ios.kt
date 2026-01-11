87434642e9807ca9b639a7731c9c171dc45ab204
r0adkll <veedubusc@gmail.com>
2026-01-11 18:36:34 -0500

    lint

--- data/network/oidc/src/iosMain/kotlin/app/campfire/network/oidc/di/OpenIdAuthComponent.ios.kt
+++ data/network/oidc/src/iosMain/kotlin/app/campfire/network/oidc/di/OpenIdAuthComponent.ios.kt
@@ -7,7 +7,10 @@ import me.tatarka.inject.annotations.Provides
 actual interface PlatformOpenIdAuthComponent {
   @Provides
   fun provideIosAuthorizationFlow(): AuthorizationFlow = object : AuthorizationFlow {
-    override suspend fun getAuthorization(serverUrl: String, extraHeaders: Map<String, String>?): Result<OpenIdAuthorization> {
+    override suspend fun getAuthorization(
+      serverUrl: String,
+      extraHeaders: Map<String, String>?,
+    ): Result<OpenIdAuthorization> {
       return Result.failure(IllegalStateException("OAuth not supported on this platform"))
     }
   }
