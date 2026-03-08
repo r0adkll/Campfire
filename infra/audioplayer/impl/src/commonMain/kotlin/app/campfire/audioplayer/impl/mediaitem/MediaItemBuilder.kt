becab35f299f9ccee1ecab95b2f9158f689133a8
r0adkll <veedubusc@gmail.com>
2026-03-07 21:27:17 -0500

    Made mediaitem builder more robust and resilient to odd item track/chapter states.

--- infra/audioplayer/impl/src/commonMain/kotlin/app/campfire/audioplayer/impl/mediaitem/MediaItemBuilder.kt
+++ infra/audioplayer/impl/src/commonMain/kotlin/app/campfire/audioplayer/impl/mediaitem/MediaItemBuilder.kt
@@ -7,7 +7,11 @@ import app.campfire.core.model.Chapter
 import app.campfire.core.model.LibraryItem
 import app.campfire.core.model.Media
 import app.campfire.core.model.Session
+import app.campfire.crashreporting.CrashReporter
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.time.Duration.Companion.seconds
 
 object MediaItemBuilder : Corked("MediaItemBuilders") {
 
@@ -25,33 +29,88 @@ object MediaItemBuilder : Corked("MediaItemBuilders") {
       }
     }
 
-    // Its probably a safe assumption that if the # of chapters matches the # of audio tracks/files
-    // then this item is file segmented by chapter and we can assume a 1:1 relationship
-    val likelyTrackPerChapter = chapters.size == audioTracks.size
+    // It's probably a safe assumption that if the # of chapters matches the # of audio tracks/files
+    // then this item is file segmented by chapter, and we can assume a 1:1 relationship
+    if (chapters.size == audioTracks.size) {
+      return audioTracks.mapIndexed { index, track ->
+        val chapter = chapters[index]
+        createMediaItem(chapter, track, false, media)
+      }
+    }
 
-    return chapters.mapIndexed { index, chapter ->
-      if (likelyTrackPerChapter) {
-        val track = audioTracks[index]
-        val trackStart = track.startOffset.seconds.inWholeSeconds
-        val trackEnd = (track.startOffset + track.duration).seconds.inWholeSeconds
-        val diff = chapter.start.seconds.inWholeSeconds in trackStart.rangeUntil(trackEnd)
-        return@mapIndexed createMediaItem(chapter, track, false, media)
+    // If there is only one track, then this is likely the other common setup where one track (i.e File)
+    // is segmented by N-Chapters. Here we can make the assumption to just
+    if (audioTracks.size == 1) {
+      val track = audioTracks.first()
+      val trackStart = track.startOffset.seconds
+      val trackEnd = (track.startOffset + track.duration).seconds
+
+      // Filter Chapter to just those that "fit" in the track
+      val chaptersForTrack = chapters.filter { chapter ->
+        val chapterStart = chapter.start.seconds
+        chapterStart in trackStart.rangeTo(trackEnd)
       }
 
-      // Chapters may not sync with audio tracks, so we should attempt to find the track
-      // that contains this chapter
-      val track = audioTracks.find {
-        val trackStart = it.startOffset.seconds.inWholeSeconds
-        val trackEnd = (it.startOffset + it.duration).seconds.inWholeSeconds
-        chapter.start.seconds.inWholeSeconds in trackStart.rangeUntil(trackEnd)
-      } ?: throwMediaItemException(index, "Unable to find track for chapter '${chapter.title}'", item)
+      // If there are fewer chapters to slice by then in the item, then the chapter meta
+      // is busted. Record an exception and then slice only the ones that matter
+      if (chaptersForTrack.size != chapters.size) {
+        recordMediaItemException("Chapters are mis-aligned and not all fit onto the track", item)
+      } else {
+        // We can assume all chapters cover this track,
+        // but let's validate that all the chapters provide full track coverage
+        val lastChapter = chapters.last()
+        val remainingTrackDuration = (track.startOffset + track.duration).seconds - lastChapter.end.seconds
+        if (remainingTrackDuration > RemainingTrackDurationThreshold) {
+          recordMediaItemException("Remaining track after last chapter is too long", item)
+        }
+      }
 
-      // Determine now if the audio track needs to be clipped for this item\
-      val diff = computerChapterTrackDiffInSeconds(chapter, track)
+      // Regardless of state, slice the track to the chapters and let the user
+      // see our error UI to correct the playback of this item
+      return@with chaptersForTrack.map { chapter ->
+        createMediaItem(chapter, track, true, media)
+      }
+    }
 
-      createMediaItem(chapter, track, true, media)
+    // If we have multiple tracks that are not the same # of chapters, then lets do our best
+    // to slice them accordingly.
+    return audioTracks.flatMap { track ->
+      // Filter chapters that _start_ in this track
+      val chaptersForTrack = chapters.filter { chapter ->
+        val chapterStart = chapter.start.seconds
+        val chapterEnd = chapter.end.seconds
+        val trackStart = track.startOffset.seconds
+        val trackEnd = (track.startOffset + track.duration).seconds
+
+        chapterStart in trackStart.rangeTo(trackEnd) ||
+          chapterEnd in trackStart.rangeTo(trackEnd)
+      }
+
+      if (chaptersForTrack.isNotEmpty()) {
+        // Validate that chapters actually cover the track.
+        val endOfChapters = chaptersForTrack.last().end.seconds
+        val remainingTrackDuration = (track.startOffset + track.duration).seconds - endOfChapters
+        if (remainingTrackDuration > RemainingTrackDurationThreshold) {
+          // If the chapter fail to fully cover the track, then just return the track and let the user fix
+          // their item on the server
+          recordMediaItemException("Remaining track after last chapter is too long", item)
+          listOf(createMediaItem(track, media))
+        } else {
+          // For each chapter filter to this track, cut a media item for each
+          chaptersForTrack.map { chapter ->
+            createMediaItem(chapter, track, true, media)
+          }
+        }
+      } else {
+        // Otherwise, if we are unable to associate chapter data to this track,
+        // just create a media item of just the track. This way we ensure playback occurs
+        recordMediaItemException("Unable to find any chapters for the track[${track.index}]", item)
+        listOf(createMediaItem(track, media))
+      }
     }
   }
+
+  private val RemainingTrackDurationThreshold = 10.seconds
 
   internal fun computerChapterTrackDiffInSeconds(
     chapter: Chapter,
@@ -73,9 +132,17 @@ object MediaItemBuilder : Corked("MediaItemBuilders") {
       uri = track.contentUrl,
       mimeType = track.mimeType,
       clipping = if (clipAudio) {
+        // There COULD be a scenario where the chapter starts before the track, but
+        // ends in the track. Therefore, we can't clip a track BEFORE it starts so
+        // make sure we start at either the chapter, or track start
+        val startSeconds = max(chapter.start, track.startOffset)
+        // There COULD be a scenario where the chapter extends beyond
+        // the duration of a track. In this case we can't clip it so
+        // make sure the end is the lesser of the two
+        val endSeconds = min(chapter.end, track.startOffset + track.duration)
         MediaItem.Clipping(
-          startMs = chapter.start.seconds.inWholeMilliseconds,
-          endMs = chapter.end.seconds.inWholeMilliseconds,
+          startMs = startSeconds.seconds.inWholeMilliseconds,
+          endMs = endSeconds.seconds.inWholeMilliseconds,
         )
       } else {
         null
@@ -128,13 +195,12 @@ object MediaItemBuilder : Corked("MediaItemBuilders") {
     )
   }
 
-  private fun throwMediaItemException(
-    chapterIndex: Int,
+  private fun recordMediaItemException(
     message: String,
     item: LibraryItem,
-  ): Nothing {
+  ) {
     // Dump item state to logs
-    ebark { "MediaItem Compute Exception @ Index[$chapterIndex]" }
+    ebark { "MediaItem Compute Exception" }
     ebark {
       """
         LibraryItem[
@@ -167,6 +233,6 @@ object MediaItemBuilder : Corked("MediaItemBuilders") {
       }
     }
 
-    throw MediaItemException(message, item)
+    CrashReporter.record(MediaItemException(message, item))
   }
 }
