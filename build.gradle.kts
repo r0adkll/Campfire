bf965186bf02d5520a90cfc853958cd569a060b8
r0adkll <veedubusc@gmail.com>
2026-03-18 21:01:11 -0400

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ plugins {
   alias(libs.plugins.firebase.crashlytics) apply false
   alias(libs.plugins.firebase.appdistribution) apply false
   alias(libs.plugins.google.services) apply false
-  alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.parcelize) apply false
   alias(libs.plugins.kotlin.serialization) apply false
   alias(libs.plugins.ksp) apply false
