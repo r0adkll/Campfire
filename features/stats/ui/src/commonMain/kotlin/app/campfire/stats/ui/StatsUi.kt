67acb098383db62e6b95ab40bf48cc3c1c8eec91
r0adkll <veedubusc@gmail.com>
2026-09-03 17:15:56 -0400

    Remove unused import after app bar padding change

--- features/stats/ui/src/commonMain/kotlin/app/campfire/stats/ui/StatsUi.kt
+++ features/stats/ui/src/commonMain/kotlin/app/campfire/stats/ui/StatsUi.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
